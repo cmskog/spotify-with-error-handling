26c0d90fa7dd702072aa8f933b34b6c0f8e3d22b
Carl Michael Skog <cm@xn--ger-pla.com>
2023-10-21 23:19:37 +0200

    Rename spotify-with-error-handling.nix to default.nix
    And default.nix to build.nix.
    Makes the callPackage invocation a bit shorter.

--- default.nix
+++ default.nix
@@ -1,2 +1,22 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.callPackage ./spotify-with-error-handling.nix {}
+{ procps, spotify, writeShellScriptBin } :
+writeShellScriptBin
+  "spotify"
+  ''
+  PATH=
+  set -euo pipefail
+  shopt -s shift_verbose
+
+  checkErrorReturn()
+  {
+    ${spotify}/bin/spotify
+    local return=$?
+
+    echo "Return: $return"
+    [[ $return == 133 ]]
+  }
+
+  while checkErrorReturn
+  do
+    ${procps}/bin/pkill -TERM '^\.spotify-wrappe' || true
+  done
+  ''
